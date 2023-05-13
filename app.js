17b322720b7929fd0a9b1786bdf38ba92b524223
iurimach <119754002+iurimach@users.noreply.github.com>
2023-05-13 18:32:11 +0400

    version1

---
+++ app.js
@@ -0,0 +1,33 @@
+var like = document.getElementById("like")
+like.addEventListener("click", myFunction)
+var likelessener= document.querySelector("#likelessener")
+var likeimg=document.querySelector("#likeimg")
+var witelilike=document.querySelector("#witelilike")
+var likdediv=document.querySelector("#likdediv")
+
+
+
+
+function myFunction(){
+
+    likelessener.textContent= "24 "
+    likeimg.style.display = "none"
+    witelilike.style.display = "block"
+    like.style.display = "none"
+    likebutton2.style.display="block"
+
+}
+
+var likebutton2=document.querySelector("#likebutton2")
+
+likebutton2.addEventListener ("click", myFunction2)
+
+function myFunction2(){
+    
+    likelessener.textContent= "23"
+    likebutton2.style.display = "none";
+    like.style.display="block" ;
+    likeimg.style.display="block"
+
+    
+}
